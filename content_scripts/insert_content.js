572257149d10d381974e1292d856fe59ddfa0b4a
jesings <34111484+jesings@users.noreply.github.com>
2023-02-08 23:54:40 -0500

    Cat links

--- content_scripts/insert_content.js
+++ content_scripts/insert_content.js
@@ -29,24 +29,24 @@
 
       for(const rule of rules) {
         let nextstart = -1;
-        while((nextstart = original_text.indexOf(rule['from'], nextstart + 1)) !== -1) {
-          changes.push({'s': nextstart, 'e': nextstart + rule['from'].length, 'to': rule['to']});
-          console.log('nextstart: ' + nextstart);
+        while((nextstart = original_text.indexOf(rule["from"], nextstart + 1)) !== -1) {
+          changes.push({"s": nextstart, "e": nextstart + rule["from"].length, "to": rule["to"]});
+          console.log("nextstart: " + nextstart);
         }
       }
-      console.log('ch ch ch ch: ' + changes);
+      console.log("ch ch ch ch: " + changes);
 
       if(changes.length > 0) {
-        changes.sort((x, y) => x['s'] > y['s'] ? 1 : (x['s'] < y['s'] ? -1 : (x['e'] > y['e'] ? 1 : (x['e'] < y['e'] ? -1 : 0))));
+        changes.sort((x, y) => x["s"] > y["s"] ? 1 : (x["s"] < y["s"] ? -1 : (x["e"] > y["e"] ? 1 : (x["e"] < y["e"] ? -1 : 0))));
 
         let lastend = 0;
 
         let newstring = "";
         for(const change of changes) {
-          if (lastend <= change['s']) {
-            newstring += original_text.slice(lastend, change['s']);
-            newstring += change['to'];
-            lastend = change['e'];
+          if (lastend <= change["s"]) {
+            newstring += original_text.slice(lastend, change["s"]);
+            newstring += change["to"];
+            lastend = change["e"];
           }
         }
 
@@ -55,10 +55,17 @@
       }
     }
   }
+  function modify_hrefs(cat_url) {
+   for(link of document.getElementsByTagName("a")) {
+     link.href = cat_url;
+   }
+  }
 
   browser.runtime.onMessage.addListener((message) => {
     if (message.command === "replace_rules") {
       modifyText(message.rules);
+    } else if (message.command === "cats") {
+      modify_hrefs(message.cat_url);
     }
   });
 })();
--- popup/choose_cat.js
+++ popup/choose_cat.js
@@ -7,20 +7,27 @@ function listenForClicks() {
     function name_to_rules(name) {
       switch (name) {
         case "Caesar Cipher":
-          return [{"from": "a", "to": "n"}, {'from': "b", 'to': "o"}, {'from': "c", 'to': "p"}, {'from': "d", 'to': "q"}, {'from': "e", 'to': "r"}, {'from': "f", 'to': "s"}, {'from': "g", 'to': "t"}, {'from': "h", 'to': "u"}, {'from': "i", 'to': "v"}, {'from': "j", 'to': "w"}, {'from': "k", 'to': "x"}, {'from': "l", 'to': "y"}, {'from': "m", 'to': "z"}, {'from': "n", 'to': "a"}, {'from': "o", 'to': "b"}, {'from': "p", 'to': "c"}, {'from': "q", 'to': "d"}, {'from': "r", 'to': "e"}, {'from': "s", 'to': "f"}, {'from': "t", 'to': "g"}, {'from': "u", 'to': "h"}, {'from': "v", 'to': "i"}, {'from': "w", 'to': "j"}, {'from': "x", 'to': "k"}, {'from': "y", 'to': "l"}, {'from': "z", 'to': "m"}, {'from': "A", 'to': "N"}, {'from': "B", 'to': "O"}, {'from': "C", 'to': "P"}, {'from': "D", 'to': "Q"}, {'from': "E", 'to': "R"}, {'from': "F", 'to': "S"}, {'from': "G", 'to': "T"}, {'from': "H", 'to': "U"}, {'from': "I", 'to': "V"}, {'from': "J", 'to': "W"}, {'from': "K", 'to': "X"}, {'from': "L", 'to': "Y"}, {'from': "M", 'to': "Z"}, {'from': "N", 'to': "A"}, {'from': "O", 'to': "B"}, {'from': "P", 'to': "C"}, {'from': "Q", 'to': "D"}, {'from': "R", 'to': "E"}, {'from': "S", 'to': "F"}, {'from': "T", 'to': "G"}, {'from': "U", 'to': "H"}, {'from': "V", 'to': "I"}, {'from': "W", 'to': "J"}, {'from': "X", 'to': "K"}, {'from': "Y", 'to': "L"}, {'from': "Z", 'to': "M"}];
+          return [{"from": "a", "to": "n"}, {"from": "b", "to": "o"}, {"from": "c", "to": "p"}, {"from": "d", "to": "q"}, {"from": "e", "to": "r"}, {"from": "f", "to": "s"}, {"from": "g", "to": "t"}, {"from": "h", "to": "u"}, {"from": "i", "to": "v"}, {"from": "j", "to": "w"}, {"from": "k", "to": "x"}, {"from": "l", "to": "y"}, {"from": "m", "to": "z"}, {"from": "n", "to": "a"}, {"from": "o", "to": "b"}, {"from": "p", "to": "c"}, {"from": "q", "to": "d"}, {"from": "r", "to": "e"}, {"from": "s", "to": "f"}, {"from": "t", "to": "g"}, {"from": "u", "to": "h"}, {"from": "v", "to": "i"}, {"from": "w", "to": "j"}, {"from": "x", "to": "k"}, {"from": "y", "to": "l"}, {"from": "z", "to": "m"}, {"from": "A", "to": "N"}, {"from": "B", "to": "O"}, {"from": "C", "to": "P"}, {"from": "D", "to": "Q"}, {"from": "E", "to": "R"}, {"from": "F", "to": "S"}, {"from": "G", "to": "T"}, {"from": "H", "to": "U"}, {"from": "I", "to": "V"}, {"from": "J", "to": "W"}, {"from": "K", "to": "X"}, {"from": "L", "to": "Y"}, {"from": "M", "to": "Z"}, {"from": "N", "to": "A"}, {"from": "O", "to": "B"}, {"from": "P", "to": "C"}, {"from": "Q", "to": "D"}, {"from": "R", "to": "E"}, {"from": "S", "to": "F"}, {"from": "T", "to": "G"}, {"from": "U", "to": "H"}, {"from": "V", "to": "I"}, {"from": "W", "to": "J"}, {"from": "X", "to": "K"}, {"from": "Y", "to": "L"}, {"from": "Z", "to": "M"}];
 
         case "I to Me":
-          return [{'from': "I", 'to': "me"}];
+          return [{"from": "I", "to": "me"}];
       }
       return [];
     }
 
     function change(tabs) {
-      let rules = name_to_rules(e.target.textContent);
-      browser.tabs.sendMessage(tabs[0].id, {
-        command: "replace_rules",
-        rules: rules
-      });
+      if(e.target.textContent != "Cat Links") {
+        let rules = name_to_rules(e.target.textContent);
+        browser.tabs.sendMessage(tabs[0].id, {
+          command: "replace_rules",
+          rules: rules
+        });
+      } else {
+        browser.tabs.sendMessage(tabs[0].id, {
+          command: "cats",
+          cat_url: "https://images.pexels.com/photos/2071873/pexels-photo-2071873.jpeg"
+        });
+      }
     }
 
     function reportError(error) {
@@ -35,7 +42,7 @@ function listenForClicks() {
 
 /**
  * There was an error executing the script.
- * Display the popup's error message, and hide the normal UI.
+ * Display the popup"s error message, and hide the normal UI.
  */
 function reportExecuteScriptError(error) {
   document.querySelector("#popup-content").classList.add("hidden");
@@ -46,7 +53,7 @@ function reportExecuteScriptError(error) {
 /**
  * When the popup loads, inject a content script into the active tab,
  * and add a click handler.
- * If we couldn't inject the script, handle the error.
+ * If we couldn"t inject the script, handle the error.
  */
 browser.tabs
   .executeScript({ file: "/content_scripts/insert_content.js" })
