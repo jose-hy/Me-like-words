405f2027d8b50e3a94c056fae05f8f3fd23556f7
Amber Li <al6862@nyu.edu>
2023-02-10 21:10:03 -0500

    more instructions

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Overview
 
-Me-like-words is a firefox extension that replaces words.
-
-# Basic Usage
-
-To words.csv, add the word to replace and the word replacing it. 
-#### Example Formatting
-from,to  
-replaceme,imreplaced  
-ex1,ex2  
-I,me
+Me-like-words is a Firefox extension that encodes and decodes a Caesar cipher, replaces "I" with "Me," replaces links to a cat photo, or does all three things at once! 
+ 
 # How to Install
 
-After changing the words.csv file, open Firefox and load the `about:debugging#/runtime/this-firefox` page. Click [Load Temporary Add-on](https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Temporary_Installation_in_Firefox) and select the `manifest.json` file within the Me-like-words folder.
+Open Firefox and load the `about:debugging#/runtime/this-firefox` page. Click [Load Temporary Add-on](https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Temporary_Installation_in_Firefox) and select the `manifest.json` file within the Me-like-words folder. 
+
+# How to Use
+
+In the extensions of the browser tool bar, find the Me-like-words drop-down menu. Click on an option, or add a custom input to replace other words. The custom input must be inputed in "Rules:" in the following formatting:
+
+from,to
+replaceme,imreplaced
+ex1,ex2
---
+++ content_scripts/insert_content.js
@@ -0,0 +1,85 @@
+(() => {
+  /**
+   * Check and set a global guard variable.
+   * If this content script is injected into the same page again,
+   * it will do nothing next time.
+   */
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
+
+  /**
+   * This function is the one that does the main work of our add-on
+   * It finds every text node in the webpage
+   * And it applies all of the rules (in the format of [from text, to text])
+   * to the page, applying the rules according to which non-overlapping rules
+   * appear first in the text.
+   */
+  function modifyText(rules) {
+    let root = document.body;
+    const it = document.createTreeWalker(root, NodeFilter.SHOW_TEXT);
+
+    let cn;
+    while ((cn = it.nextNode())) {
+      const originalText = cn.nodeValue;
+
+      let changes = [];
+
+      for(const rule of rules) {
+        let nextstart = -1;
+        while((nextstart = originalText.indexOf(rule["from"], nextstart + 1)) !== -1) {
+          changes.push({"s": nextstart, "e": nextstart + rule["from"].length, "to": rule["to"]});
+        }
+      }
+
+      if(changes.length > 0) {
+        changes.sort((x, y) => x["s"] > y["s"] ? 1 : (x["s"] < y["s"] ? -1 : (x["e"] > y["e"] ? 1 : (x["e"] < y["e"] ? -1 : 0))));
+
+        let lastend = 0;
+
+        let newstring = "";
+        for(const change of changes) {
+          if (lastend <= change["s"]) {
+            newstring += originalText.slice(lastend, change["s"]);
+            newstring += change["to"];
+            lastend = change["e"];
+          }
+        }
+
+        newstring += originalText.slice(lastend);
+        cn.nodeValue = newstring;
+      }
+    }
+  }
+
+  function modifyHrefs(catURL) {
+   for(let link of document.getElementsByTagName("a")) {
+     link.href = catURL;
+   }
+  }
+
+  function preprocessCustomRules(oldRules) {
+    let newRules = []
+    for(let i = 0; i < oldRules.length; i++) {
+      const rule = oldRules[i];
+      if(rule.length != 2) {
+        alert("Custom rules for replacement specified are malformed in rule " + i);
+        return undefined;
+      }
+      newRules.push({"from": rule[0], "to": rule[1]});
+    }
+    return newRules;
+  }
+
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "Rules") {
+      modifyText(message.rules);
+    } else if (message.command === "Custom") {
+      const newRules = preprocessCustomRules(message.rules);
+      modifyText(newRules);
+    } else if (message.command === "Cat Links") {
+      modifyHrefs(message.rules);
+    }
+  });
+})();
